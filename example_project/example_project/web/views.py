ce95f3eb584eaea97fcd5c52f49532ee35fcb15c
Andrei Antoukh <niwi@niwi.be>
2012-01-23 20:15:10 +0100

    Add more complex structures to templates for benchmarking.

--- example_project/example_project/web/views.py
+++ example_project/example_project/web/views.py
@@ -4,14 +4,15 @@ from django.views.generic import View
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+lista = [{'id':x, 'pow': x*x} for x in xrange(20)]
 
 class Test1(View):
     def get(self, request):
-        return render_to_response("home.jinja", {},
+        return render_to_response("home.jinja", {'lista':lista},
             context_instance=RequestContext(request))
 
 
 class Test2(View):
     def get(self, request):
-        return render_to_response("home.html", {},
+        return render_to_response("home.html", {'lista':lista},
             context_instance=RequestContext(request))
