d7e4b205ecd7dfaa732cde46774ad3be883695bc
Wouter Lansu <wfrlansu@gmail.com>
2013-05-17 13:24:36 +0200

    added KEEP_DJANGO_TEMPLATES settings variable

--- django_jinja/loaders.py
+++ django_jinja/loaders.py
@@ -2,18 +2,25 @@
 from django.conf import settings
 from django.template import TemplateDoesNotExist
 from django.template.loaders import app_directories
+from django.template.loaders.app_directories import Loader as AppDirectoriesLoader
 from django.template.loaders import filesystem
 
 from django_jinja.base import env
 import jinja2
 
 DEFAULT_JINJA2_TEMPLATE_EXTENSION = getattr(settings,
-    'DEFAULT_JINJA2_TEMPLATE_EXTENSION', '.jinja')
+
 
 class LoaderMixin(object):
     is_usable = True
 
     def load_template(self, template_name, template_dirs=None):
+        app_directories_loader = AppDirectoriesLoader()
+        for skip_path in getattr(settings, 'KEEP_DJANGO_TEMPLATES', []):
+            # Hackish way to use Django Loader for Django Admin
+            if skip_path in template_name:
+                return app_directories_loader(template_name, template_dirs)
+
         if not template_name.endswith(DEFAULT_JINJA2_TEMPLATE_EXTENSION):
             return super(LoaderMixin, self).load_template(template_name, template_dirs)
 
