f715b19f7bf9883d48bb4fc1c3b6c03a9f4b7f8c
Vadim Statishin <statishin@gmail.com>
2013-12-01 16:35:51 +0200

    Suppress exception NoReverseMatch for django reverse url and notify via logging system.

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,4 +19,5 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^test1/$', Test1.as_view(), name='test-1'),
     url(r'^test2/$', Test2.as_view(), name='test-2'),
+    url(r'^test3/$', TemplateView.as_view(template_name='test3.jinja'), name='test-3')
 )
