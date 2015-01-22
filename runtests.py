f4012e31499db90d0f8f5e0bbd43fd3d2e4c6d3a
Andrey Antukh <niwi@niwi.be>
2015-01-22 16:47:15 +0100

    Normalize runtests code style and adapt it to test application renaming (in previous commit)

--- runtests.py
+++ runtests.py
@@ -6,65 +6,66 @@ import django
 from django.conf import settings
 from django.core.management import call_command
 
-TEST_TEMPLATE_DIR = 'templates'
+TEST_TEMPLATE_DIR = "templates"
 RUNTESTS_DIR = os.path.dirname(__file__)
 sys.path.insert(0, "testing")
 
 
 test_settings = {
-    'DATABASES':{
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
+    "DATABASES":{
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
         }
     },
-    'INSTALLED_APPS': [
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.staticfiles',
-        'django.contrib.messages',
-        'django_jinja',
-        'django_jinja_test',
-        'pipeline',
-        'django_jinja.contrib._pipeline',
+    "INSTALLED_APPS": [
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.staticfiles",
+        "django.contrib.messages",
+        "django_jinja",
+        "testapp",
+        "pipeline",
+        "django_jinja.contrib._pipeline",
     ],
-    'INTERNAL_IPS': ('127.0.0.1',),
-    'ROOT_URLCONF':'django_jinja_test.urls',
-    'STATIC_URL':'/static/',
-    'STATIC_ROOT': os.path.join(RUNTESTS_DIR, 'static'),
-    'TEMPLATE_DIRS':(
+    "INTERNAL_IPS": ("127.0.0.1",),
+    "ROOT_URLCONF":"testapp.urls",
+    "STATIC_URL":"/static/",
+    "STATIC_ROOT": os.path.join(RUNTESTS_DIR, "static"),
+    "TEMPLATE_DIRS":(
         os.path.join(RUNTESTS_DIR, TEST_TEMPLATE_DIR),
     ),
-    'USE_I18N': True,
-    'USE_TZ': True,
-    'LANGUAGE_CODE':'en',
-    'MIDDLEWARE_CLASSES': (
-        'django.middleware.common.CommonMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
+    "USE_I18N": True,
+    "USE_TZ": True,
+    "LANGUAGE_CODE":"en",
+    "MIDDLEWARE_CLASSES": (
+        "django.middleware.common.CommonMiddleware",
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
     ),
-    'MANAGERS': ("niwi@niwi.be",),
-    'TEMPLATE_LOADERS': [
-        'django_jinja.loaders.AppLoader',
-        'django_jinja.loaders.FileSystemLoader',
+    "MANAGERS": ("niwi@niwi.be",),
+    "TEMPLATE_LOADERS": [
+        "django_jinja.loaders.AppLoader",
+        "django_jinja.loaders.FileSystemLoader",
     ],
-    'PIPELINE_CSS': {
-        'test': {
-            'source_filenames': ["style.css"],
-            'output_filename': "style.2.css",
+    "PIPELINE_CSS": {
+        "test": {
+            "source_filenames": ["style.css"],
+            "output_filename": "style.2.css",
         }
     },
-    'PIPELINE_JS': {
-        'test': {
-            'source_filenames': ['script.js'],
-            'output_filename': 'script.2.js',
+    "PIPELINE_JS": {
+        "test": {
+            "source_filenames": ["script.js"],
+            "output_filename": "script.2.js",
         }
     },
-    'JINJA2_CONSTANTS': {"foo": "bar"},
-    'JINJA2_MUTE_URLRESOLVE_EXCEPTIONS': True,
+    "JINJA2_CONSTANTS": {"foo": "bar"},
+    "JINJA2_MUTE_URLRESOLVE_EXCEPTIONS": True,
 }
+
 
 if django.VERSION[:2] >= (1, 6):
     test_settings["TEST_RUNNER"] = "django.test.runner.DiscoverRunner"
@@ -78,6 +79,6 @@ if __name__ == "__main__":
 
     args = sys.argv
     args.insert(1, "test")
-    args.insert(2, "django_jinja_test")
+    args.insert(2, "testapp")
 
     execute_from_command_line(args)
