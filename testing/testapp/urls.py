0aafae4b1ed3d29bf7d7afce0758e0a8a88c9f66
Andrey Antukh <niwi@niwi.be>
2015-02-17 15:42:09 +0100

    Add better test views.

--- testing/testapp/urls.py
+++ testing/testapp/urls.py
@@ -4,11 +4,13 @@ from django.conf.urls import patterns, include, url
 from django_jinja import views
 
 from .views import BasicTestView
+from .views import PipelineTestView
 
-urlpatterns = patterns('',
-    url(r'^test1/$', BasicTestView.as_view(), name='test-1'),
-    url(r'^test1/(?P<data>\d+)/$', BasicTestView.as_view(), name='test-1'),
-    url(r'^test/404$', views.PageNotFound.as_view(), name="page-404"),
-    url(r'^test/403$', views.PermissionDenied.as_view(), name="page-403"),
-    url(r'^test/500$', views.ServerError.as_view(), name="page-500"),
+urlpatterns = patterns("",
+    url(r"^test1/$", BasicTestView.as_view(), name="test-1"),
+    url(r"^test1/(?P<data>\d+)/$", BasicTestView.as_view(), name="test-1"),
+    url(r"^test-pipeline/$", PipelineTestView.as_view(), name="pipeline-test"),
+    url(r"^test/404$", views.PageNotFound.as_view(), name="page-404"),
+    url(r"^test/403$", views.PermissionDenied.as_view(), name="page-403"),
+    url(r"^test/500$", views.ServerError.as_view(), name="page-500"),
 )
--- testing/testapp/views.py
+++ testing/testapp/views.py
@@ -2,10 +2,13 @@
 
 from django.views.generic import View
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 
 class BasicTestView(View):
     def get(self, request, data=None):
-        return HttpResponse("Hello World")
+        return render_to_response("hello_world.jinja", {"name": "Jinja2"})
 
-
+class PipelineTestView(View):
+    def get(self, request, data=None):
+        return render_to_response("pipeline_test.jinja")
