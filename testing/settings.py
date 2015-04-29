df622a21bc2030239a7297879a07a0501e70a671
Andrey Antukh <niwi@niwi.be>
2015-04-29 19:22:36 +0200

    Other cosemetic fixes on testing settings.

--- testing/settings.py
+++ testing/settings.py
@@ -83,24 +83,31 @@ JINJA2_EXTENSIONS = DEFAULT_EXTENSIONS + [
 ]
 
 TEMPLATES = [
-   {"BACKEND": "django_jinja.backend.Jinja2",
-    "NAME": "jinja2",
-    "APP_DIRS": True,
-    "OPTIONS": {
-        "context_processors": [
-           "django.contrib.auth.context_processors.auth",
-           "django.template.context_processors.debug",
-           "django.template.context_processors.i18n",
-           "django.template.context_processors.media",
-           "django.template.context_processors.static",
-           "django.template.context_processors.tz",
-           "django.contrib.messages.context_processors.messages",
-        ],
-        "constants": {
-            "foo": "bar",
-        },
-        "extensions": JINJA2_EXTENSIONS
-    }}
+    {
+        "BACKEND": "django_jinja.backend.Jinja2",
+        "NAME": "jinja2",
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+            ],
+            "constants": {
+                "foo": "bar",
+            },
+            "extensions": JINJA2_EXTENSIONS
+        }
+    },
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True
+    }
 ]
 
 PIPELINE_CSS = {
