6a6b7ea53919a40f91f22299ac5c6de208a34324
Andrey Antukh <niwi@niwi.nz>
2015-11-13 12:39:59 +0200

    Remove useless conditional setup in apps initialization.

--- django_jinja/apps.py
+++ django_jinja/apps.py
@@ -10,11 +10,3 @@ class DjangoJinjaAppConfig(AppConfig):
 
     def ready(self):
         base.patch_django_for_autoescape()
-
-        # This is because django apps is introduced
-        # in django 1.7 but the support for multiple
-        # engines is introduced in django 1.8. So
-        # in django 1.7 we should initialize the global
-        # jinja environment.
-        if django.VERSION[:2] == (1, 7):
-            base.setup()
