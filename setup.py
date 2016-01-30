5f3e4ce6c3098cf4c0341488a03a021b32f5f0c7
Andrey Antukh <niwi@niwi.nz>
2016-01-30 23:02:43 +0200

    Set version to 2.1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name = "django-jinja",
-    version = "2.1.1",
+    version = "2.1.2",
     description = "Jinja2 templating language integrated in Django.",
     long_description = "",
     keywords = "django, jinja2",
