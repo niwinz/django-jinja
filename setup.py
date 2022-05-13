ac6d1cfae2471081384111421a89a4fbe60fa3c4
Phillip Marshall <wizpig64@users.noreply.github.com>
2022-05-12 17:11:18 -0700

    v2.10.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = "django-jinja",
-    version = "2.10.1",
+    version = "2.10.2",
     description = "Jinja2 templating language integrated in Django.",
     long_description = open("README.rst").read(),
     long_description_content_type='text/x-rst',
