6b0db8e481892f405927d97a1e6fafef420d0107
Phillip Marshall <wizpig64@users.noreply.github.com>
2022-09-04 06:27:35 -0700

    Django 4.1 (#297)
    * Drop Python 3.6, Django 2.2 support for 3.7, 3.2 or higher
    
    * Add Django 4.1 support, testing Python 3.11 support
    
    * make gh-actions use python 3.11-dev

--- setup.py
+++ setup.py
@@ -31,10 +31,10 @@ setup(
         "django_jinja.views.generic",
     ],
     include_package_data = True,
-    python_requires = ">=3.6",
+    python_requires = ">=3.7",
     install_requires = [
         "jinja2>=3",
-        "django>=2.2",
+        "django>=3.2",
     ],
     tests_require = [
         "pytz",
@@ -42,20 +42,20 @@ setup(
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
-        "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Internet :: WWW/HTTP",
     ]
 )
